87ca10cd35441075c73e2b289072631a592d134d
Samadhan Maske <samadhanmaske208@gmail.com>
2023-10-24 22:15:29 +0530

    Controlled componets Basic form handling

--- src/App.js
+++ src/App.js
@@ -1,1 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Form from './Components/Form';
+import LifeCycleA from './Components/LifeCycleA';
 
+function App() {
+  return (
+    <div className="App">
+      {/* <Form/> */}
+      <LifeCycleA/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Form.js
@@ -0,0 +1,72 @@
+
+import React, { Component } from 'react'
+
+class Form extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            username:'',
+            comment:'',
+            topic:'react',
+        }
+    }
+    
+    handleUsername = (event)=>{
+        this.setState({
+            username: event.target.value,
+        })
+    }
+
+    handleComment = (event)=>{
+        this.setState({
+            comment: event.target.value
+        })
+    }
+
+    handleTopic = (event)=>{
+        this.setState({
+            topic: event.target.value
+        })
+    }
+
+    handleSubmit = (event)=>{
+        alert(`${this.state.username} ${this.state.comment} ${this.state.topic}`);
+        event.preventDefault();
+    }
+
+    handleTopic = (event)=>{
+        this.setState({
+            topic:event.target.value,
+        })
+    }
+
+    render() {
+        return (
+        <div>
+            <form onSubmit={this.handleSubmit}>
+                <div>
+                    <label>Username </label>
+                    <input type='text' value={this.state.username} onChange={this.handleUsername} />
+                </div>
+                <div>
+                    <label>Comment </label>
+                    <input type='textarea' value={this.state.comment} onChange={this.handleComment} />
+                </div>
+                <div>
+                    <label>Topic </label>
+                    <select value={this.state.topic} onChange={this.handleTopic}>
+                        <option value="react">React</option>
+                        <option value="angular">Angular</option>
+                        <option value="vue">Vue</option>
+
+                    </select>
+                </div>
+                <button type='submit'>Submit</button>
+            </form>
+        </div>
+        )
+    }
+}
+
+export default Form
